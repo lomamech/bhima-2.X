third-culture-software/bhima

test(implemented full tests for Payroll by Index module) - Verified the correct transition of BHIMA system to Payroll by Index mode. - Added and executed unit tests (client/server) for all core payroll modules: - functions, grades, holidays, offdays, iprTax, payrollConfig, rubrics, rubricConfig, accountConfig, weekendConfig, employeeConfig. - Implemented and validated E2E tests for: - Payroll creation and payment via Multiple Payroll (Indice). - Proper setup of the Indice Payroll Rubrics and configuration consistency. - Added integration tests for staffingIndices and staffingIndicesReport modules. - Verified CRUD operations for payroll-related entities (Grade, Function, Rubric, Configurations). - Confirmed correct calculation of: Base Index, Worked Days, Daily Index, Adjusted Index, Gross Salary, INSS, IPR, and Net Salary. - Ensured that all test suites pass locally, including complex payroll scenarios with multiple employees. - Verified system transition to “Payroll by Index” mode - Added unit tests for functions - Added unit and E2E tests for job titles - Added unit tests for grades - Added unit tests for holidays - Added unit tests for offdays - Added unit tests for IPR tax - Added unit tests for payroll configuration - Added unit tests for rubrics - Added unit tests for rubric configuration - Added unit tests for account configuration - Added unit tests for weekend configuration - Added unit tests for employee configuration - Added unit tests for multiple payroll processing - Added unit tests for multiple payroll by index - Added unit tests for staffing indices - Updated payroll tests to retrieve the latest enterprise settings directly from the database

test/server-unit/payroll_test_unit/function.spec.js
@@ -1,0 +1,118 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+// Import the database module to mock it
+const db = require('../../../server/lib/db');
+
+const controller = require('../../../server/controllers/admin/functions');
+
+describe('test/server-unit/payroll-test-unit/function', () => {
+
+  // reusable mocks
+  let req;
+  let res;
+  let next;
+
+  beforeEach(() => {
+    req = { params : {}, body : {} };
+    res = {
+      status : sinon.stub().returnsThis(),
+      json : sinon.stub(),
+    };
+    next = sinon.spy();
+  });
+
+  afterEach(() => {
+    sinon.restore(); // reset everything after each test
+  });
+
+  it('list() should return a status 200 with the list of functions', async () => {
+    const fonctionData = [
+      { id : 1, fonction_txt : 'Medecin', numEmployees : 4 },
+      { id : 2, fonction_txt : 'Infirmier', numEmployees : 3 },
+    ];
+
+    sinon.stub(db, 'exec').resolves(fonctionData);
+
+    await controller.list(req, res, next);
+
+    expect(db.exec.calledOnce).to.equal(true);
+    expect(res.status.calledWith(200)).to.equal(true);
+    expect(res.json.calledWith(fonctionData)).to.equal(true);
+  });
+
+  it('detail() must return a function by Id', async () => {
+    const record = { id : 1, fonction_txt : 'Chirurgien' };
+    sinon.stub(db, 'one').resolves(record);
+    req.params.id = 1;
+
+    await controller.detail(req, res, next);
+
+    expect(db.one.calledOnce).to.equal(true);
+    expect(res.status.calledWith(200)).to.equal(true);
+    expect(res.json.calledWith(record)).to.equal(true);
+  });
+
+  it('detail() must call next(e) if DB error', async () => {
+    const fakeError = new Error('DB error');
+    sinon.stub(db, 'one').rejects(fakeError);
+    req.params.id = 1;
+
+    await controller.detail(req, res, next);
+
+    expect(next.calledWith(fakeError)).to.equal(true);
+  });
+
+  // ---------------------------------------------------------
+  it('create() must insert a new function and return 201', async () => {
+    const insertResult = { insertId : 99 };
+    sinon.stub(db, 'exec').resolves(insertResult);
+
+    req.body = { fonction_txt : 'Radiologue' };
+
+    await controller.create(req, res, next);
+
+    expect(db.exec.calledOnce).to.equal(true);
+    expect(res.status.calledWith(201)).to.equal(true);
+    expect(res.json.calledWith({ id : 99 })).to.equal(true);
+  });
+
+  // ---------------------------------------------------------
+  it('update() must modify and return the updated function', async () => {
+    const updated = { id : 10, fonction_txt : 'Biologiste' };
+    sinon.stub(db, 'exec').resolves();
+    sinon.stub(db, 'one').resolves(updated);
+
+    req.params.id = 10;
+    req.body = { fonction_txt : 'Biologiste' };
+
+    await controller.update(req, res, next);
+
+    expect(db.exec.calledOnce).to.equal(true);
+    expect(res.status.calledWith(200)).to.equal(true);
+    expect(res.json.calledWith(updated)).to.equal(true);
+  });
+
+  it('update() must call next(e) if the database returns an error', async () => {
+    const fakeError = new Error('Erreur SQL');
+    sinon.stub(db, 'exec').rejects(fakeError);
+    req.params.id = 10;
+
+    await controller.update(req, res, next);
+
+    expect(next.calledWith(fakeError)).to.equal(true);
+  });
+
+  // ---------------------------------------------------------
+  it('delete() must call db.delete() with the correct arguments', () => {
+    const deleteStub = sinon.stub(db, 'delete');
+    req.params.id = 5;
+
+    controller.delete(req, res, next);
+
+    expect(deleteStub.calledOnce).to.equal(true);
+    expect(deleteStub.firstCall.args[0]).to.equal('fonction');
+    expect(deleteStub.firstCall.args[1]).to.equal('id');
+    expect(deleteStub.firstCall.args[2]).to.equal(5);
+  });
+});

lomamech/bhima-2.X

test/server-unit/payroll_test_unit/function.spec.js
@@ -1,0 +1,124 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+// Import the database module to mock it
+const db = require('../../../server/lib/db');
+
+const controller = require('../../../server/controllers/admin/functions');
+
+describe('test/server-unit/payroll-test-unit/function', () => {
+
+  // reusable mocks
+  let req;
+  let res;
+  let next;
+
+  beforeEach(() => {
+    req = { params : {}, body : {} };
+    res = {
+      status : sinon.stub().returnsThis(),
+      json : sinon.stub(),
+    };
+    next = sinon.spy();
+  });
+
+  afterEach(() => {
+    sinon.restore(); // reset everything after each test
+  });
+
+  it('list() should return a status 200 with the list of functions', async () => {
+    const fonctionData = [
+      { id : 1, fonction_txt : 'Medecin', numEmployees : 4 },
+      { id : 2, fonction_txt : 'Infirmier', numEmployees : 3 },
+    ];
+
+    sinon.stub(db, 'exec').resolves(fonctionData);
+
+    await controller.list(req, res, next);
+
+    expect(db.exec.calledOnce).to.equal(true);
+    expect(res.status.calledWith(200)).to.equal(true);
+    expect(res.json.calledWith(fonctionData)).to.equal(true);
+  });
+
+  it('detail() must return a function by Id', async () => {
+    const record = { id : 1, fonction_txt : 'Chirurgien' };
+    sinon.stub(db, 'one').resolves(record);
+    req.params.id = 1;
+
+    await controller.detail(req, res, next);
+
+    expect(db.one.calledOnce).to.equal(true);
+    expect(res.status.calledWith(200)).to.equal(true);
+    expect(res.json.calledWith(record)).to.equal(true);
+  });
+
+  it('detail() should throw if a DB error occurs', async () => {
+    const fakeError = new Error('DB error');
+    sinon.stub(db, 'one').rejects(fakeError);
+    req.params.id = 1;
+
+    try {
+      await controller.detail(req, res);
+      throw new Error('Expected error was not thrown');
+    } catch (err) {
+      expect(err).to.equal(fakeError);
+    }
+  });
+
+  // ---------------------------------------------------------
+  it('create() must insert a new function and return 201', async () => {
+    const insertResult = { insertId : 99 };
+    sinon.stub(db, 'exec').resolves(insertResult);
+
+    req.body = { fonction_txt : 'Radiologue' };
+
+    await controller.create(req, res, next);
+
+    expect(db.exec.calledOnce).to.equal(true);
+    expect(res.status.calledWith(201)).to.equal(true);
+    expect(res.json.calledWith({ id : 99 })).to.equal(true);
+  });
+
+  // ---------------------------------------------------------
+  it('update() must modify and return the updated function', async () => {
+    const updated = { id : 10, fonction_txt : 'Biologiste' };
+    sinon.stub(db, 'exec').resolves();
+    sinon.stub(db, 'one').resolves(updated);
+
+    req.params.id = 10;
+    req.body = { fonction_txt : 'Biologiste' };
+
+    await controller.update(req, res, next);
+
+    expect(db.exec.calledOnce).to.equal(true);
+    expect(res.status.calledWith(200)).to.equal(true);
+    expect(res.json.calledWith(updated)).to.equal(true);
+  });
+
+  it('update() should throws an error', async () => {
+    const fakeError = new Error('SQL Error');
+    sinon.stub(db, 'exec').rejects(fakeError);
+    req.params.id = 10;
+
+    try {
+      await controller.detail(req, res);
+      throw new Error('Expected error was not thrown');
+    } catch (err) {
+      expect(err).to.equal(fakeError);
+    }
+  });
+
+  // ---------------------------------------------------------
+  it('delete() must call db.delete() with the correct arguments', () => {
+    const deleteStub = sinon.stub(db, 'delete');
+    req.params.id = 5;
+
+    controller.delete(req, res, next);
+
+    expect(deleteStub.calledOnce).to.equal(true);
+    expect(deleteStub.firstCall.args[0]).to.equal('fonction');
+    expect(deleteStub.firstCall.args[1]).to.equal('id');
+    expect(deleteStub.firstCall.args[2]).to.equal(5);
+  });
+});