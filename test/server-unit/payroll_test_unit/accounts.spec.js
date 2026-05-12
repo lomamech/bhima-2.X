741d3bcef94f1582af9f52c6ccf3dea9e75c59ca
Jonathan Niles <jonathanwniles@gmail.com>
2026-05-12 14:45:45 -0500

    ci: setuid on semaphoreci
    This commit attempts to make the tests pass on SemaphoreCI which now
    fails due to chromium download issues.

--- test/server-unit/payroll_test_unit/accounts.spec.js
+++ test/server-unit/payroll_test_unit/accounts.spec.js
@@ -59,7 +59,7 @@ describe('test/server-unit/payroll-test-unit/accounts', () => {
     sinon.stub(db, 'one').rejects(new Error('DB error'));
     req.params.id = 22;
 
-    expect(controller.detail(req, res)).to.be.rejectedWith('DB error');
+    expect(await controller.detail(req, res)).to.be.rejectedWith('DB error');
   });
 
   // ---------------------------------------------------------
